d83150e01b2ca557f91d6834470f568b1d04f892
Spich9215 <wspichiger@gmail.com>
2025-08-14 09:29:53 -0400

    Updated YAML - github actions workflow

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,7 +6,7 @@ const Header: React.FC = () => {
   return (
     <header className="header-container">
       <div className="header-content">
-        <h3 className="site-title">Will Spichiger's Site</h3>
+        <h3 className="site-title">Will Spichiger's Personal Site</h3>
       </div>
       {/* You can add more navigation links here if needed, or keep them in App.tsx */}
     </header>
--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -13,23 +13,7 @@ const Home: React.FC = () => {
         <br></br>
         I am hoping that I can showcase some technology projects and find a fulfilling employment opportunity.
       </p>
-      <p>
-        Personally, I am a skeptic of social media and the intentions underlying the act of broadcasting personal information. 
-        Though I have little to hide the abundance of data that FAANG has access to about me gives me pause for concern.
-        Mostly I figure they simply want to profit off of my data and attention, however, I believe that FAANG holds the power to influence me psychologically and consequently manipulate my emotions/behavior/actions.
-        I think this personal site will enable me to be the slightest bit be less subjected to corporate control and retain a smither of ownership over my digital life albeit at the cost of the network. 
-        <br></br>
-        One benefit of creating this is that I have not had to check any terms and conditions or privacy policies, nor do I have to worry about being tracked by cookies or be subjected to ads.
-        <br></br>
-        I can guarantee that you will not be subjected to ads (save my seeking employment). 
-        <br></br>
-        <br></br>
-        I may get like 5 viewers for the rest of my lifetime and they may all be me. 
-        <br></br>
-        This site is built on AWS Infrastructure (Amazon), developed in VS Code on a machine running Windows, leverages open-source code... so "no man is an island." 
-        Clearly, just the privilege of being able to make something like this demonstrates that I stand on the shoulders of giants (not corporate ones so much as intellectual ones albeit w/some overlap)).
-      </p>
-      {/* You can add more content here, like a brief intro to yourself */}
+      
     </div>
   );
 };
